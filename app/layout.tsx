a7cf9a680895bd99c92410e489bc2c9ae83a4818
Shehzad-Rahim <shehzadrahim33@gmail.com>
2025-02-08 02:41:42 +0500

    name update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Q3 Quiz Prep',
+  title: 'Q2 Quiz Prep',
   description: 'Created by Shehzad Rahim',
 };
 
